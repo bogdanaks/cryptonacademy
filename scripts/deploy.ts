e903178d7553a74256ef8c357d73de49cf08b4ce
bogdanaksenov <bogdanaks@bk.ru>
2022-02-04 00:28:01 +0300

    Update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Donation = await ethers.getContractFactory("Donation");
+  const donation = await Donation.deploy();
+  await donation.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Donation deployed to:", donation.address);
 }
 
 main().catch((error) => {
