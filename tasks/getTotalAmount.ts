83cac0878288c0d8a85b7d85f2c0a4127504bb0f
bogdanaksenov <bogdanaks@bk.ru>
2022-02-04 22:56:22 +0300

    Add param address

--- tasks/getTotalAmount.ts
+++ tasks/getTotalAmount.ts
@@ -4,13 +4,15 @@ import "@nomiclabs/hardhat-ethers";
 
 interface GetDonatersArgs {
   contract: string;
+  address: string;
 }
 
 task("get-total-amount", "Get total amount")
   .addParam("contract", "Contract address")
+  .addParam("address", "Search address")
   .setAction(async (args: GetDonatersArgs, hre) => {
     const Donation = await hre.ethers.getContractAt("Donation", args.contract);
-    const totalAmount = await Donation.getTotalAmount();
+    const totalAmount = await Donation.getTotalAmount(args.address);
 
     console.log(`Total amount: ${totalAmount}`);
   });
