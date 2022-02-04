ceb33bbfac21e48f5e03c51cf71855b845b19440
bogdanaksenov <bogdanaks@bk.ru>
2022-02-04 23:31:04 +0300

    Change network on rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,8 +17,8 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.11",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RENKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
